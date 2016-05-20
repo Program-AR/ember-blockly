47298176cac3cdd61abca4bac3b4c6b500864efa
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-20 12:06:39 -0300

    Simplificando test.

--- tests/integration/components/ember-blockly-test.js
+++ tests/integration/components/ember-blockly-test.js
@@ -11,14 +11,6 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{ember-blockly}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.ok(this.$());
 
-  // Template block usage:"
-  this.render(hbs`
-    {{#ember-blockly}}
-      template block text
-    {{/ember-blockly}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
