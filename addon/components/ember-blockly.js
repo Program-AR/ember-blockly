73ba78c4513b02890a48da9c973c9350f15fb812
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-30 15:42:08 -0300

    update

--- addon/components/ember-blockly.js
+++ addon/components/ember-blockly.js
@@ -5,6 +5,26 @@ export default Ember.Component.extend({
   layout,
   didInsertElement() {
     let toolboxElement = this.$().find('#toolbox')[0];
-    let workspace = Blockly.inject('blocklyDiv', {toolbox: toolboxElement});
+
+    /*
+    var toolbox = '<xml>';
+    toolbox += '  <block type="controls_if"></block>';
+    toolbox += '  <sep gap="8"></sep>';
+    toolbox += '  <block type="controls_whileUntil"></block>';
+    toolbox += '</xml>';
+    */
+
+    //debugger;
+    //let workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox,
+    let workspace = Blockly.inject('blocklyDiv', {toolbox: toolboxElement,
+      zoom:
+               {controls: true,
+                wheel: false,
+                startScale: 1.0,
+                maxScale: 3,
+                minScale: 0.3,
+                scaleSpeed: 1.2},
+           trashcan: true
+    });
   }
 });
