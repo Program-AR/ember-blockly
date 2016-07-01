e2a1f10d5ddc469b7860580e17efbf1a70a22cc0
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-07-01 14:30:46 -0300

    Creando una forma de separar en secciones sin colapsar.

--- addon/components/ember-blockly.js
+++ addon/components/ember-blockly.js
@@ -3,28 +3,56 @@ import layout from '../templates/components/ember-blockly';
 
 export default Ember.Component.extend({
   layout,
+
   didInsertElement() {
-    let toolboxElement = this.$().find('#toolbox')[0];
 
-    /*
-    var toolbox = '<xml>';
-    toolbox += '  <block type="controls_if"></block>';
-    toolbox += '  <sep gap="8"></sep>';
-    toolbox += '  <block type="controls_whileUntil"></block>';
-    toolbox += '</xml>';
-    */
+    this.createSection("section_control", "Control");
+    this.createSection("section_logic", "Lógica");
 
-    //debugger;
-    //let workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox,
-    let workspace = Blockly.inject('blocklyDiv', {toolbox: toolboxElement,
-      zoom:
-               {controls: true,
-                wheel: false,
-                startScale: 1.0,
-                maxScale: 3,
-                minScale: 0.3,
-                scaleSpeed: 1.2},
-           trashcan: true
+
+    let toolbox = this.createToolbox([
+      'section_control', 'controls_if', 'controls_if_else', 'controls_whileUntil',
+      'section_logic', 'logic_compare', 'logic_operation', 'logic_boolean']);
+
+    let options = {
+       toolbox: toolbox,
+       trashcan: true,
+       zoom: {
+         controls: true,
+         wheel: false,
+         startScale: 1.0,
+         maxScale: 3,
+         minScale: 0.3,
+         scaleSpeed: 1.2
+       },
+     };
+
+    let workspace = Blockly.inject('blocklyDiv', options);
+  },
+
+  createSection(name, label) {
+    Blockly.Blocks[name] = {
+      init: function() {
+        this.appendDummyInput().appendField(`  ${label}  `);
+        this.setOutput(false);
+        this.setNextStatement(false);
+        this.setPreviousStatement(false);
+        this.setDisabled(true);
+      }
+    };
+  },
+
+  createToolbox(bloques) {
+    let toolbox = [];
+
+    toolbox.push('<xml>');
+
+    bloques.forEach((bloque) => {
+      toolbox.push(`<block type="${bloque}"></block>`);
     });
+
+    toolbox.push('</xml>');
+
+    return toolbox.join("\n");
   }
 });
